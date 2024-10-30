c71b8aa52300f9682e8b8ae9a69a66aa0fca0324
anna carolina <141137890+annacarolina051c@users.noreply.github.com>
2024-10-30 12:17:25 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Badminton',
+    'O que é Baminton?',
+    'O badminton é um esporte de raquete jogado em duplas ou individualmente, onde o objetivo é acertar uma peteca sobre a rede e fazê-lo cair na quadra adversária. É rápido e ágil, exigindo boa reflexo e estratégia.'
+)
+
+criaCartao(
+    'Badminton',
+    'Qual a posição dentro de quadra?',
+    '1- INDIVIDUAL: cada jogador ocupa sua metade de quadra e 2- DUPLAS: jogadores podem ficar lado a lado ou um atrás do outro, deprendendo da estratégia.'
+)
+
+criaCartao(
+    'Badminton',
+    'O que é criar uma estratégia dentro de quadra?',
+    'Criar uma estratégia no badminton envolve planejar movimentação, ataque e defesa, considerando as habilidades dos adversários. Isso inclui escolher golpes, posicionar-se bem e antecipar jogadas para aumentar as chances de pontuar.'
+)
+
+criaCartao(
+    'Badminton',
+    'Como fazer golpes dentro de quadra?',
+    '1-Smash: Bater forte de cima para baixo. 2-Clear: Jogar a peteca para o fundo da quadra adversária. 3-Drop: Soltar a peteca logo acima da rede. 4-Drive: Bater a peteca em linha reta e rápido'
+)
